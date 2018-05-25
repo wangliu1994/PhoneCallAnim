d834c84974b1fed856a174f0497ce106068b1bd7
王柳 <wangliu@zbj.com>
2018-05-25 16:19:23 +0800

    sync

---
+++ app/src/main/java/com/winnie/phonecallanim/MainActivity.java
@@ -0,0 +1,123 @@
+package com.winnie.phonecallanim;
+
+import android.animation.Animator;
+import android.animation.Keyframe;
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView image;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        image = (ImageView) findViewById(R.id.image);
+        /**
+         * 左右震动效果
+         */
+        Keyframe frame0 = Keyframe.ofFloat(0f, 0);
+        Keyframe frame1 = Keyframe.ofFloat(0.1f, -20f);
+        Keyframe frame2 = Keyframe.ofFloat(0.2f, 20f);
+        Keyframe frame3 = Keyframe.ofFloat(0.3f, -20f);
+        Keyframe frame4 = Keyframe.ofFloat(0.4f, 20f);
+        Keyframe frame5 = Keyframe.ofFloat(0.5f, -20f);
+        Keyframe frame6 = Keyframe.ofFloat(0.6f, 20f);
+        Keyframe frame7 = Keyframe.ofFloat(0.7f, -20f);
+        Keyframe frame8 = Keyframe.ofFloat(0.8f, 20f);
+        Keyframe frame9 = Keyframe.ofFloat(0.9f, -20f);
+        Keyframe frame10 = Keyframe.ofFloat(1, 0);
+        PropertyValuesHolder frameHolder1 = PropertyValuesHolder.ofKeyframe("rotation", frame0, frame1, frame2, frame3, frame4,frame5, frame6, frame7, frame8, frame9, frame10);
+
+
+/**
+ * scaleX放大1.1倍
+ */
+        Keyframe scaleXframe0 = Keyframe.ofFloat(0f, 1);
+        Keyframe scaleXframe1 = Keyframe.ofFloat(0.1f, 1.1f);
+        Keyframe scaleXframe2 = Keyframe.ofFloat(0.2f, 1.1f);
+        Keyframe scaleXframe3 = Keyframe.ofFloat(0.3f, 1.1f);
+        Keyframe scaleXframe4 = Keyframe.ofFloat(0.4f, 1.1f);
+        Keyframe scaleXframe5 = Keyframe.ofFloat(0.5f, 1.1f);
+        Keyframe scaleXframe6 = Keyframe.ofFloat(0.6f, 1.1f);
+        Keyframe scaleXframe7 = Keyframe.ofFloat(0.7f, 1.1f);
+        Keyframe scaleXframe8 = Keyframe.ofFloat(0.8f, 1.1f);
+        Keyframe scaleXframe9 = Keyframe.ofFloat(0.9f, 1.1f);
+        Keyframe scaleXframe10 = Keyframe.ofFloat(1, 1);
+        PropertyValuesHolder frameHolder2 = PropertyValuesHolder.ofKeyframe("ScaleX",scaleXframe0,scaleXframe1,scaleXframe2,scaleXframe3,scaleXframe4,scaleXframe5,scaleXframe6,scaleXframe7,scaleXframe8,scaleXframe9,scaleXframe10);
+
+
+/**
+ * scaleY放大1.1倍
+ */
+        Keyframe scaleYframe0 = Keyframe.ofFloat(0f, 1);
+        Keyframe scaleYframe1 = Keyframe.ofFloat(0.1f, 1.1f);
+        Keyframe scaleYframe2 = Keyframe.ofFloat(0.2f, 1.1f);
+        Keyframe scaleYframe3 = Keyframe.ofFloat(0.3f, 1.1f);
+        Keyframe scaleYframe4 = Keyframe.ofFloat(0.4f, 1.1f);
+        Keyframe scaleYframe5 = Keyframe.ofFloat(0.5f, 1.1f);
+        Keyframe scaleYframe6 = Keyframe.ofFloat(0.6f, 1.1f);
+        Keyframe scaleYframe7 = Keyframe.ofFloat(0.7f, 1.1f);
+        Keyframe scaleYframe8 = Keyframe.ofFloat(0.8f, 1.1f);
+        Keyframe scaleYframe9 = Keyframe.ofFloat(0.9f, 1.1f);
+        Keyframe scaleYframe10 = Keyframe.ofFloat(1, 1);
+        PropertyValuesHolder frameHolder3 = PropertyValuesHolder.ofKeyframe("ScaleY",scaleYframe0,scaleYframe1,scaleYframe2,scaleYframe3,scaleYframe4,scaleYframe5,scaleYframe6,scaleYframe7,scaleYframe8,scaleYframe9,scaleYframe10);
+
+/**
+ * 构建动画
+ */
+        final Animator animator = ObjectAnimator.ofPropertyValuesHolder(image, frameHolder1,frameHolder2,frameHolder3);
+        animator.setDuration(1000);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animator.start();
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
